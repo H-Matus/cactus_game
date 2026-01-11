efb32988dd9769dd0c2d5d55d989effd85559db1
Henrikas Matusevicius <hm294@st-andrews.ac.uk>
2026-01-11 00:53:46 +0000

    feat(cli): Complete initial version CLI.

--- src/main/java/com/example/cactus_game/ui/CliUI.java
+++ src/main/java/com/example/cactus_game/ui/CliUI.java
@@ -1,12 +1,46 @@
 
-package ui;
+package com.example.cactus_game.ui;
+
+import com.example.cactus_game.game.Cactus;
+import java.util.Scanner;
 
 public class CliUI {
-  public CliUI() {
-    System.out.println("UI is initiated.");
-  }
+  public CliUI() {}
 
   public void start() {
-    System.out.println("Test");
+    boolean exit = false;
+
+    while (exit != true) {
+      printMenu();
+
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+
+      switch (input) {
+        case "1":
+          Cactus cactus = new Cactus();
+          cactus.start();
+          break;
+        case "2":
+          System.out.println("Exiting the program.");
+          exit = true;
+          scanner.close();
+          break;
+        default:
+          System.out.println("Wrong input.");
+          break;
+      }
+    }
+  }
+
+  private void printMenu() {
+    System.out.println("-------------------");
+    System.out.println("Menu");
+    System.out.println("-------------------");
+    System.out.println("1. Cactus");
+    System.out.println("2. Exit");
+    System.out.println("-------------------");
+    System.out.print("Please insert the number of the option:");
+    System.out.println("");
   }
 }
